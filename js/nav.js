7073b8274898d33769549a21a22895d6f29bff26
Juan Carlos Mendoza Tasayco <juancarlosmendozatasayco148-dotcom@users.noreply.github.com>
2026-06-25 23:51:35 -0500

    fix: cache-bust navbar fetch to avoid GitHub Pages CDN cache

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Cargar el navbar
-    fetch('./components/navbar.html')
+    fetch('./components/navbar.html?v=' + new Date().getTime())
         .then(response => response.text())
         .then(data => {
             document.body.insertAdjacentHTML('afterbegin', data);
